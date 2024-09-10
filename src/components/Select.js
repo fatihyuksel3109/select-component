c35033ff482882e033eec1a3d02b77f5263232ed
Fatih Yuksel <fatihyuksel3109@gmail.com>
2024-09-10 22:57:58 +0300

    icons ara moved to icons folder

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,17 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
+import ChevronDownIcon from './Icons/ChevronDownIcon';
+import CheckIcon from './Icons/CheckIcon';
 import './Select.css';
 
-const ChevronDownIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polyline points="6 9 12 15 18 9"></polyline>
-  </svg>
-);
-
-const CheckIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polyline points="20 6 9 17 4 12"></polyline>
-  </svg>
-);
 
 const Select = ({ 
   options, 
